eadc7a584f77897e0fb6e084d1a759cf85395049
BhaskarGpta <bhaskargpt456@gmail.com>
2025-05-02 16:08:06 +0530

    emotional comment

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def main():
     BT_7274.start_chat()
     
     print("Chatbot conversation ended. Exiting...")
+    print("Hope you enjoyed the conversation! Have a great day ahead! 😊") #make user experience more personalised
     print("Just remember you always have a buddy to help you out! 🤖") #make user experience even more personalised
 
 if __name__ == "__main__":
